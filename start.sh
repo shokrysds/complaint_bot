933afba1f3cc1d3940aefc693212a102fb9fb061
shokrysds <shokrysds@gmail.com>
2025-08-11 00:10:18 +0330

    fix Render package install issue

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-python3 -m pip install --upgrade pip
-python3 -m pip install -r requirements.txt
+python3 -m pip install --upgrade pip --break-system-packages
+python3 -m pip install -r requirements.txt --break-system-packages
 python3 bot.py
